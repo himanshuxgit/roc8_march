c5f9bd62d0ce550b39810a19e1db7b5981a97cf2
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2024-03-17 08:50:00 +0530

    Create login api

---
+++ src/pages/api/auth/login/route.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// type ResponseData = {
+//   message: string;
+// };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { email, password } = await JSON.parse(req.body);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+      password: password,
+    },
+  });
+  console.log("11111111111", user);
+  
+
+  if (!user) {
+    return res.status(400).send({ message: "User not found" });
+  }
+
+  res.status(200).json({ message: {"id": user.id, "name": user.name} });
+}
